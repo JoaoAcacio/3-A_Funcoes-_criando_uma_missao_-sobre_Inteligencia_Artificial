ac2827e0b632fa97f14308b28720d654e66f33c5
JoaoAcacio <103216129+JoaoAcacio@users.noreply.github.com>
2024-09-16 15:59:16 -0300

    Add files via upload

--- Funções criando uma missão sobre Inteligência Artificial/main.js
+++ Funções criando uma missão sobre Inteligência Artificial/main.js
@@ -102,7 +102,7 @@ function mostraAlternativas() {
     }
 }
 function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacoes;
+    const afirmacoes = opcaoSelecionada.afirmacao;
     historiaFinal += afirmacoes + "";
     atual++;
     mostraPergunta();
